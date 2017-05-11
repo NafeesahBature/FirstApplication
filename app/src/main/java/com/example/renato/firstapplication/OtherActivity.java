d1947d7c2e82fd6edd51e7521eb9126ba4c78ff9
Besenczi Renátó <renato.besenczi@gmail.com>
2017-05-11 10:44:30 +0200

    Some change.

--- app/src/main/java/com/example/renato/firstapplication/OtherActivity.java
+++ app/src/main/java/com/example/renato/firstapplication/OtherActivity.java
@@ -1,42 +1,26 @@
 package com.example.renato.firstapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-    void startOtherActivity(){
-        startActivity(new Intent(this, OtherActivity.class));
-    }
+public class OtherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_other);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Snackbar.make(view, "Button anything", Snackbar.LENGTH_LONG)
-                //        .setAction("Action", null).show();
-                startOtherActivity();
-            }
-        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "This is the other activity", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
